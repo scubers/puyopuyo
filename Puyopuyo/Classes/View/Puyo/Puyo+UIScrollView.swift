e2b1bddd84aa42a7f840b2b45a16fd9778cdff58
Jrwong <jr-wong@qq.com>
2020-03-20 02:03:50 +0800

    feat: 曾对scrollview添加几个puyo方法

--- Puyopuyo/Classes/View/Puyo/Puyo+UIScrollView.swift
+++ Puyopuyo/Classes/View/Puyo/Puyo+UIScrollView.swift
@@ -31,7 +31,7 @@ public extension Puyo where T: UIScrollView {
         }
         return self
     }
-    
+
     @discardableResult
     func scrollEnabled<O: Outputing>(_ value: O) -> Self where O.OutputType == Bool {
         value.safeBind(to: view, id: #function) { v, a in
@@ -39,7 +39,7 @@ public extension Puyo where T: UIScrollView {
         }
         return self
     }
-    
+
     @discardableResult
     func showHorzIndicator<O: Outputing>(_ value: O) -> Self where O.OutputType == Bool {
         value.safeBind(to: view, id: #function) { v, a in
@@ -47,7 +47,7 @@ public extension Puyo where T: UIScrollView {
         }
         return self
     }
-    
+
     @discardableResult
     func showVertIndicator<O: Outputing>(_ value: O) -> Self where O.OutputType == Bool {
         value.safeBind(to: view, id: #function) { v, a in
@@ -55,7 +55,7 @@ public extension Puyo where T: UIScrollView {
         }
         return self
     }
-    
+
     @discardableResult
     func pagingEnabled<O: Outputing>(_ value: O) -> Self where O.OutputType == Bool {
         value.safeBind(to: view, id: #function) { v, a in
@@ -65,21 +65,67 @@ public extension Puyo where T: UIScrollView {
     }
 
     @discardableResult
+    func contentInsets(all: CGFloat? = nil, top: CGFloat? = nil, left: CGFloat? = nil, bottom: CGFloat? = nil, right: CGFloat? = nil) -> Self {
+        if let v = all { view.contentInset = UIEdgeInsets(top: v, left: v, bottom: v, right: v) }
+        if let v = top { view.contentInset.top = v }
+        if let v = left { view.contentInset.right = v }
+        if let v = bottom { view.contentInset.bottom = v }
+        if let v = right { view.contentInset.right = v }
+        return self
+    }
+
+    @discardableResult
+    func contentOffset(x: CGFloat? = nil, y: CGFloat? = nil) -> Self {
+        if let v = x { view.contentOffset.x = v }
+        if let v = y { view.contentOffset.y = v }
+        return self
+    }
+
+    @discardableResult
+    func contentSize(width: CGFloat? = nil, height: CGFloat? = nil) -> Self {
+        if let v = width { view.contentSize.width = v }
+        if let v = height { view.contentSize.height = v }
+        return self
+    }
+
+    @discardableResult
+    func onContentOffsetChanged<T: Inputing>(_ input: T) -> Self where T.InputType == CGPoint {
+        view.py_observing(for: #keyPath(UIScrollView.contentOffset))
+            .map { (x: CGPoint?) in x ?? .zero }
+            .distinct()
+            .safeBind(to: view) {
+                input.input(value: $1)
+            }
+        return self
+    }
+
+    @discardableResult
+    func onContentSizeChanged<T: Inputing>(_ input: T) -> Self where T.InputType == CGSize {
+        view.py_observing(for: #keyPath(UIScrollView.contentOffset))
+            .map { (x: CGSize?) in x ?? .zero }
+            .distinct()
+            .safeBind(to: view) {
+                input.input(value: $1)
+            }
+        return self
+    }
+
+    @discardableResult
     func flatBox(_ direction: Direction) -> Puyo<FlatBox> {
         if direction == .y {
             view.attach().alwaysVertBounds(true)
             return
                 FlatBox().attach(view)
-                .direction(direction)
-                .autoJudgeScroll(true)
-                .size(.fill, .wrap)
+                    .direction(direction)
+                    .autoJudgeScroll(true)
+                    .size(.fill, .wrap)
         } else {
             view.attach().alwaysHorzBounds(true)
             return
                 FlatBox().attach(view)
-                .direction(direction)
-                .autoJudgeScroll(true)
-                .size(.wrap, .fill)
+                    .direction(direction)
+                    .autoJudgeScroll(true)
+                    .size(.wrap, .fill)
         }
     }
 }
