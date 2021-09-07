473c2c141513b236c2b29fc8894603f64a675684
Jrwong <wangjunren@youzan.com>
2021-09-07 10:07:36 +0800

    fix: 修复distinct skip take的副作用

--- Puyopuyo/Classes/IO/OutputingModifier.swift
+++ Puyopuyo/Classes/IO/OutputingModifier.swift
@@ -12,8 +12,8 @@ public protocol OutputingModifier {}
 public extension OutputingModifier where Self: Outputing {
     func bind<T>(_ action: @escaping (OutputType, Inputs<T>) -> Void) -> Outputs<T> {
         Outputs<T>({ i -> Disposer in
-            self.outputing { v in
-                action(v, i)
+            self.outputing {
+                action($0, i)
             }
         })
     }
@@ -37,51 +37,51 @@ public extension OutputingModifier where Self: Outputing {
     }
 
     func ignore(_ condition: @escaping (OutputType, OutputType) -> Bool) -> Outputs<OutputType> {
-        var last: OutputType!
-        return bind { v, i in
-            guard last != nil else {
+        Outputs { i in
+            var last: OutputType?
+            return self.outputing { v in
+                if let lastValue = last {
+                    if !condition(lastValue, v) {
+                        i.input(value: v)
+                    }
+                } else {
+                    i.input(value: v)
+                }
                 last = v
-                i.input(value: v)
-                return
-            }
-            let ignore = condition(last, v)
-            last = v
-            if !ignore {
-                i.input(value: v)
             }
         }
     }
 
     func take(_ count: Int) -> Outputs<OutputType> {
-        var times: Int = 0
-        return bind { v, i in
-            guard times <= count else { return }
-            times += 1
-            i.input(value: v)
+        Outputs { i in
+            var times: Int = 0
+            return self.outputing { v in
+                guard times <= count else { return }
+                i.input(value: v)
+                times += 1
+            }
         }
     }
 
     func skip(_ count: Int) -> Outputs<OutputType> {
-        var times = 0
-        return bind { v, i in
-            guard times > count else {
+        Outputs { i in
+            var times: Int = 0
+            return self.outputing { v in
+                guard times > count else { return }
+                i.input(value: v)
                 times += 1
-                return
             }
-            i.input(value: v)
         }
     }
 
     func then<R>(_ then: @escaping (OutputType) -> Outputs<R>) -> Outputs<R> {
-        bind { o, i in
-            _ = then(o).outputing { v in
-                i.input(value: v)
-            }
+        bind {
+            _ = then($0).send(to: $1)
         }
     }
 }
 
-public extension Outputs where OutputType: OptionalableValueType {
+public extension OutputingModifier where Self: Outputing, Self.OutputType: OptionalableValueType {
     func map<R>(_ keyPath: KeyPath<OutputType.Wrap, R>, _ default: R) -> Outputs<R?> {
         bind {
             if let v = $0.optionalValue {
@@ -91,9 +91,7 @@ public extension Outputs where OutputType: OptionalableValueType {
             }
         }
     }
-}
 
-public extension Outputs where OutputType: OptionalableValueType {
     func unwrap(or: OutputType.Wrap) -> Outputs<OutputType.Wrap> {
         bind { v, i in
             if let v = v.optionalValue {
@@ -105,7 +103,7 @@ public extension Outputs where OutputType: OptionalableValueType {
     }
 }
 
-public extension Outputs where OutputType: Equatable {
+public extension OutputingModifier where Self: Outputing, Self.OutputType: Equatable {
     func distinct() -> Outputs<OutputType> {
         ignore { $0 == $1 }
     }
