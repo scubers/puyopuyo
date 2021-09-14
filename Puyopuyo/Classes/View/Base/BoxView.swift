129fc5a181d89779a3b19f411163274c0d612b95
Jrwong <wangjunren@youzan.com>
2021-09-14 13:46:16 +0800

    optimize: format code

--- Puyopuyo/Classes/View/Base/BoxView.swift
+++ Puyopuyo/Classes/View/Base/BoxView.swift
@@ -10,9 +10,9 @@ import Foundation
 open class BoxView<RegulatorType: Regulator>: UIView, Boxable {
     public var control = BoxControl<RegulatorType>()
 
-    public var regulator: RegulatorType {
-        return py_measure as! RegulatorType
-    }
+    public var regulator: RegulatorType { py_measure as! RegulatorType }
+
+    // MARK: - init
 
     override public init(frame: CGRect) {
         super.init(frame: frame)
@@ -24,13 +24,15 @@ open class BoxView<RegulatorType: Regulator>: UIView, Boxable {
         buildBody()
     }
 
-    override open func addSubview(_ view: UIView) {
-        super.addSubview(view)
-        control.setNeedsLayout(view: view, regulator: regulator)
-    }
+    // MARK: - custom method
 
+    /// override this method to build custom view in subclass
+    open func buildBody() {}
+
+    // MARK: - Overrides
+
+    /// when layout once it will be false
     private var initializing = true
-    private var layouting = false
 
     override open func setNeedsLayout() {
         if !initializing, !layouting {
@@ -39,11 +41,15 @@ open class BoxView<RegulatorType: Regulator>: UIView, Boxable {
         }
     }
 
+    /// indicate if view is calling method layoutSubviews()
+    private var layouting = false
+
     override open func layoutSubviews() {
         if layouting { return }
         layouting = true
-        super.layoutSubviews()
+
         control.layoutSubviews(view: self, regulator: regulator)
+
         initializing = false
         layouting = false
     }
@@ -52,38 +58,7 @@ open class BoxView<RegulatorType: Regulator>: UIView, Boxable {
         return control.sizeThatFits(size, regulator: regulator)
     }
 
-    open func buildBody() {}
-
     override open func didMoveToSuperview() {
-        super.didMoveToSuperview()
         control.didMoveToSuperview(view: self, regulator: regulator)
-    }
-}
-
-class InspectView: UIView {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = UIColor.blue.withAlphaComponent(0.2)
-        py_measure.activated = false
-    }
-
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError()
-    }
-
-    var superMeasure: Measure?
-
-    var disposer: Disposer?
-
-    override func didMoveToSuperview() {
-        super.didMoveToSuperview()
-        disposer?.dispose()
-        superMeasure = superview?.py_measure
-        superview?.py_boundsState().safeBind(to: self) { this, rect in
-            let margin = this.superMeasure!.margin
-            this.frame.size = CGSize(width: rect.width + margin.getHorzTotal(), height: rect.height + margin.getVertTotal())
-            this.center = CGPoint(x: rect.width / 2, y: rect.height / 2)
-        }
     }
 }
