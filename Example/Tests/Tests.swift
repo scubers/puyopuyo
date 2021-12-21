c982cee33ddd4f6feef816e108934777eaf93f9d
Jrwong <jr-wong@qq.com>
2021-12-21 21:13:13 +0800

    feat: add tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,337 +3,240 @@ import SnapKit
 import TangramKit
 import XCTest
 
+extension UIView {
+    var fw: CGFloat { frame.width }
+    var fh: CGFloat { frame.height }
+    var fx: CGFloat { frame.origin.x }
+    var fy: CGFloat { frame.origin.y }
+}
+
 class Tests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-//        XCTAssert(true, "Pass")
-        let diff = Diff(
-            src: [0, 1, 2].map { $0.description },
-            dest: [2, 0, 4, 6, 1].map { $0.description },
-            identifier: { $0 }
-        )
-        diff.check()
-        print("insert: \(diff.insert)")
-        print("delete: \(diff.delete)")
-        print("move: \(diff.move)")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            for _ in 0 ..< 1 {
-                // puyo
-//                let cell = ListCell(style: .value1, reuseIdentifier: "1")
-                // tg
-//                let cell = ListCell2(style: .value1, reuseIdentifier: "1")
-                // autolayout
-//                let cell = ListCell3(style: .value1, reuseIdentifier: "1")
-                
-                let count = 50
-                let cell = TestPuyoView(count: count)
-//                let cell = TestAutoLayout(count: count)
-                cell.state.input(value: ListData(name: "slkdjflksdjflkjsdf", text: "来看房来看房龙看房龙蛋飞龙扽静", time: "lskdj"))
-                _ = cell.sizeThatFits(CGSize(width: 320, height: 0))
-//                _ = cell.systemLayoutSizeFitting(CGSize(width: 320, height: 0))
-            }
+
+    // MARK: Measure
+
+    func testActive() {
+        let state = State(false)
+        var v1: UIView!
+        let box = HBox().attach {
+            v1 = UILabel().attach($0)
+                .text("i am label 1")
+                .width(.wrap(grow: 1))
+                .activated(state)
+                .view
         }
-    }
-}
+        .view
 
-enum Util {
-    static func randomColor() -> UIColor {
-        let red = CGFloat(arc4random()%256)/255.0
-        let green = CGFloat(arc4random()%256)/255.0
-        let blue = CGFloat(arc4random()%256)/255.0
-        let c = UIColor(red: red, green: green, blue: blue, alpha: 0.7)
-        return c
-    }
-    
-    static func randomViewColor(view: UIView) {
-        view.subviews.forEach { v in
-            v.backgroundColor = self.randomColor()
-            self.randomViewColor(view: v)
-        }
-    }
-    
-    static func random<T>(array: [T]) -> T {
-        let index = arc4random_uniform(UInt32(array.count))
-        return array[Int(index)]
-    }
-}
+        box.layoutIfNeeded()
 
-struct ListData {
-    var name: String?
-    var text: String?
-    var time: String?
-}
+        XCTAssertTrue(v1.py_measure.activated == false)
+        XCTAssertTrue(box.frame == .zero)
 
-class BaseCell: UITableViewCell, Stateful {
-    var state = State<ListData?>(nil)
-    
-    var name: Outputs<String?> {
-        return self.state.asOutput().map { $0?.name }
+        state.value = true
+
+        box.layoutIfNeeded()
+
+        XCTAssertTrue(v1.py_measure.activated == true)
+        XCTAssertTrue(box.frame != .zero)
     }
 
-    var textData: Outputs<String?> {
-        return self.state.asOutput().map { $0?.text }
+    // MARK: LinearBox
+
+    func testLBWidthFixed() {
+        var v1: UIView!
+        var v2: UIView!
+        let box = HBox().attach {
+            v1 = UIView().attach($0).width(100).view
+            v2 = UIView().attach($0).width(100).view
+        }
+        .view
+
+        box.layoutIfNeeded()
+        XCTAssertTrue(v1.fw == 100)
+        XCTAssertTrue(v2.fw == 100)
     }
 
-    var time: Outputs<String?> {
-        return self.state.asOutput().map { $0?.time }
-    }
-}
-
-class TestPuyoView: BaseCell {
-    init(count: Int) {
-        super.init(style: .default, reuseIdentifier: "slkdfjlskdjfl")
-        for i in 0 ..< count {
-            contentView.attach {
-                UILabel().attach($0)
-                    .text("slkdjflskjdflksdjflksdjf\(i)")
-            }
+    func testLBWidthRatio() {
+        var v1: UIView!
+        var v2: UIView!
+        let box = HBox().attach {
+            v1 = UILabel().attach($0).width(.fill).view
+            v2 = UILabel().attach($0).width(.fill).view
         }
-    }
+        .width(100)
+        .view
 
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError()
-    }
-}
+        box.layoutIfNeeded()
 
-class TestAutoLayout: BaseCell {
-    init(count: Int) {
-        super.init(style: .default, reuseIdentifier: "slkdfjlskdjfl")
-        for i in 0 ..< count {
-            var last: UILabel?
-            contentView.attach {
-                let v = UILabel().attach($0)
-                    .text("slkdjflskjdflksdjflksdjf\(i)")
-                    .view
-                
-                v.snp.makeConstraints { m in
-                    if let last = last {
-                        m.top.equalTo(last.snp.bottom)
-                    } else {
-                        m.top.equalToSuperview()
-                    }
-                    
-                    if i == count - 1 {
-                        m.bottom.equalToSuperview()
-                    }
-                }
-                
-                last = v
-            }
-        }
+        XCTAssertTrue(v1.fw == 50)
+        XCTAssertTrue(v2.fw == 50)
     }
 
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError()
-    }
-}
+    func testLBWidthWrap() {
+        var v1: UIView!
+        var v2: UIView!
+        let box = HBox().attach {
+            v1 = UILabel().attach($0).text("i am label 1").view
+            v2 = UILabel().attach($0).text("i am label 2").view
+        }
+        .view
 
-class ListCell: BaseCell {
-    private var root: UIView!
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.root =
-            HBox().attach(contentView) {
-                UIImageView().attach($0)
-                    .size(30, 30)
-                
-                VBox().attach($0) {
-                    HBox().attach($0) {
-                        UILabel().attach($0)
-                            .text(self.name)
-                            .size(.fill, .fill)
-                        
-                        UIButton().attach($0)
-                            .text(State("广告"))
-                            .size(80, 25)
-                    }
-                    .size(.fill, 30)
-                    
-                    UILabel().attach($0)
-                        .text(self.textData)
-                        .numberOfLines(State(0))
-                        .size(.fill, .wrap)
-                    
-                    UIButton().attach($0)
-                        .size(.wrap, 25)
-                    
-                    UIView().attach($0)
-                        .height(20)
-                        .width($0.py_boundsState().map { $0.width * 0.5 })
-                    
-                    HBox().attach($0) {
-                        UILabel().attach($0)
-                            .text(self.time)
-                        UILabel().attach($0)
-                            .text(self.time)
-                        UILabel().attach($0)
-                            .text(self.time)
-                    }
-                    .format(.between)
-                    .size(.fill, 30)
-                    
-                    HBox().attach($0) {
-                        UILabel().attach($0)
-                            .text(self.time)
-                            .size(.fill, .fill)
-                        UIButton().attach($0)
-                            .size(50, .fill)
-                    }
-                    .size(.fill, 25)
-                }
-                .size(.fill, .wrap)
-            }
-            .padding(all: 20)
-            .size(.fill, .wrap)
-            .view
-        
-        Util.randomViewColor(view: self)
+        box.layoutIfNeeded()
+
+        XCTAssertTrue(box.fw == v1.fw + v2.fw)
+        XCTAssertTrue(box.fh == max(v1.fh, v2.fh))
     }
 
-    @available(*, unavailable)
-    required init?(coder aDecoder: NSCoder) {
-        fatalError()
-    }
-    
-    override func sizeThatFits(_ size: CGSize) -> CGSize {
-        return self.root.sizeThatFits(size)
-    }
-}
+    func testLBWidthWrapPriority() {
+        var v1: UIView!
+        var v2: UIView!
+        var v3: UIView!
+        let box = HBox().attach {
+            v1 = UILabel().attach($0)
+                .text("i am label 1")
+                .width(.wrap(priority: 1))
+                .view
+            v2 = UILabel().attach($0)
+                .text("i am label 1")
+                .width(.wrap(priority: 2))
+                .view
+            v3 = UILabel().attach($0)
+                .text("i am label 1")
+                .width(.wrap(priority: 3))
+                .view
+        }
+        .width(200)
+        .view
 
-class ListCell3: BaseCell {
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        let img = UIImageView()
-        contentView.addSubview(img)
-        img.snp.makeConstraints { m in
-            m.width.height.equalTo(40)
-            m.top.left.equalToSuperview().inset(20)
-        }
-        
-        let container = UIView()
-        contentView.addSubview(container)
-        container.snp.makeConstraints { m in
-            m.left.equalTo(img.snp.right)
-            m.top.right.bottom.equalToSuperview()
-        }
-        
-        let name = UILabel()
-        container.addSubview(name)
-        Puyo(name).text(self.name)
-        name.snp.makeConstraints { m in
-            m.left.equalToSuperview()
-            m.top.equalToSuperview().inset(20)
-            m.height.equalTo(25)
-        }
-        
-        let ad = UIButton()
-        container.addSubview(ad)
-        ad.snp.makeConstraints { m in
-            m.top.right.equalToSuperview()
-        }
-        
-        let text = UILabel()
-        container.addSubview(text)
-        text.sizeToFit()
-        text.attach().text(self.textData)
-        text.snp.makeConstraints { m in
-            m.left.right.equalToSuperview()
-            m.top.equalTo(name.snp.bottom)
-        }
-        
-        let download = UIButton()
-        container.addSubview(download)
-        download.snp.makeConstraints { m in
-            m.top.equalTo(text.snp.bottom)
-            m.height.equalTo(25)
-        }
-        
-        let space = UILabel()
-        container.addSubview(space)
-        space.snp.makeConstraints { m in
-            m.height.equalTo(20)
-            m.width.equalToSuperview().multipliedBy(0.5)
-            m.top.equalTo(download.snp.bottom)
-        }
-        
-        let spread = UIView()
-        container.addSubview(spread)
-        spread.snp.makeConstraints { m in
-            m.left.right.equalToSuperview()
-            m.height.equalTo(30)
-            m.top.equalTo(space.snp.bottom)
-        }
-        
-        let v1 = UILabel()
-        let v2 = UILabel()
-        let v3 = UILabel()
-        v1.attach().text(self.textData)
-        v2.attach().text(self.textData)
-        v3.attach().text(self.textData)
-        spread.addSubview(v1)
-        spread.addSubview(v2)
-        spread.addSubview(v3)
-        
-        v1.snp.makeConstraints { m in
-            m.top.bottom.equalToSuperview()
-            m.left.equalToSuperview()
-            m.width.equalToSuperview().multipliedBy(0.3)
-        }
-        v2.snp.makeConstraints { m in
-            m.top.bottom.equalToSuperview()
-            m.center.equalToSuperview()
-            m.width.equalToSuperview().multipliedBy(0.3)
-        }
-        v3.snp.makeConstraints { m in
-            m.top.bottom.equalToSuperview()
-            m.right.equalToSuperview()
-            m.width.equalToSuperview().multipliedBy(0.3)
-        }
-        
-        let time = UILabel()
-        container.addSubview(time)
-        time.attach().text(self.time)
-        time.snp.makeConstraints { m in
-            m.left.equalToSuperview()
-            m.bottom.equalToSuperview().inset(20)
-            m.height.equalTo(25)
-            m.top.equalTo(spread.snp.bottom)
-        }
-        
-        let more = UIButton()
-        container.addSubview(more)
-        more.snp.makeConstraints { m in
-            m.right.bottom.equalToSuperview()
-            m.centerY.equalTo(time)
-        }
-        
-        Util.randomViewColor(view: contentView)
+        box.layoutIfNeeded()
+
+        XCTAssertTrue(box.fw == v1.fw + v2.fw + v3.fw)
+        XCTAssertTrue(v1.fw <= v2.fw)
+        XCTAssertTrue(v2.fw <= v3.fw)
     }
 
-    @available(*, unavailable)
-    required init?(coder aDecoder: NSCoder) {
-        fatalError()
+    func testLBWidthWrapShrink() {
+        var v1: UIView!
+        var v2: UIView!
+        var v3: UIView!
+        let box = HBox().attach {
+            v1 = UILabel().attach($0)
+                .text("i am label 1")
+                .width(.wrap(shrink: 1))
+                .view
+            v2 = UILabel().attach($0)
+                .text("i am label 1")
+                .width(.wrap(shrink: 1))
+                .view
+            v3 = UILabel().attach($0)
+                .text("i am label 1")
+                .width(.wrap(shrink: 1))
+                .view
+        }
+        .width(200)
+        .view
+
+        box.layoutIfNeeded()
+
+        XCTAssertTrue(box.fw == v1.fw + v2.fw + v3.fw)
+        XCTAssertTrue(v1.fw == v2.fw)
+        XCTAssertTrue(v3.fw == v2.fw)
     }
-    
-    override func systemLayoutSizeFitting(_ targetSize: CGSize) -> CGSize {
-        return contentView.systemLayoutSizeFitting(targetSize)
+
+    func testLBWidthWrapGrow() {
+        var v1: UIView!
+        var v2: UIView!
+        var v3: UIView!
+        let box = HBox().attach {
+            v1 = UILabel().attach($0)
+                .text("i am label 1")
+                .width(.wrap(grow: 1))
+                .view
+            v2 = UILabel().attach($0)
+                .text("i am label 1")
+                .width(.wrap(grow: 2))
+                .view
+            v3 = UILabel().attach($0)
+                .text("i am label 1")
+                .width(.wrap(grow: 3))
+                .view
+        }
+        .width(1000)
+        .view
+
+        box.layoutIfNeeded()
+
+        XCTAssertTrue(box.fw == v1.fw + v2.fw + v3.fw)
+        XCTAssertTrue(v1.fw < v2.fw)
+        XCTAssertTrue(v3.fw > v2.fw)
     }
+
+    func testLBWidthWrapMaxMin() {
+        var v1: UIView!
+        var v2: UIView!
+        var v3: UIView!
+        let box = HBox().attach {
+            v1 = UILabel().attach($0)
+                .text("i am label 1")
+                .width(.wrap(max: 20))
+                .view
+            v2 = UILabel().attach($0)
+                .text("33")
+                .width(.wrap(min: 100))
+                .view
+            v3 = UILabel().attach($0)
+                .text("i am label 1")
+                .width(.wrap)
+                .view
+        }
+        .view
+
+        box.layoutIfNeeded()
+
+        XCTAssertTrue(box.fw == v1.fw + v2.fw + v3.fw)
+        XCTAssertTrue(v1.fw == 20)
+        XCTAssertTrue(v2.fw == 100)
+    }
+
+    func testLBViewAspectRatio() {
+        var v1: UIView!
+        var v2: UIView!
+        var v3: UIView!
+        let box = HBox().attach {
+            v1 = UILabel().attach($0)
+                .text("i am label 1")
+                .aspectRatio(1 / 2)
+                .view
+            v2 = UILabel().attach($0)
+                .text("i am label 1")
+                .aspectRatio(1)
+                .view
+            v3 = UILabel().attach($0)
+                .text("i am label 1")
+                .aspectRatio(3 / 1)
+                .view
+        }
+        .view
+
+        box.layoutIfNeeded()
+
+        XCTAssertTrue(v1.fw / v1.fh == 1 / 2)
+        XCTAssertTrue(v2.fw / v2.fh == 1)
+        XCTAssertTrue(v3.fw / v3.fh == 3 / 1)
+
+        XCTAssertTrue(box.fw == v1.fw + v2.fw + v3.fw)
+        XCTAssertTrue(box.fh == [v1.fh, v2.fh, v3.fh].max())
+    }
+
+    // MARK: FlowBox
+
+    // MARK: ZBox
 }
